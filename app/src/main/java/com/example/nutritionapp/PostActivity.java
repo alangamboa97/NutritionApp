b0281f9fea2b39740fbdd433a732f8031cb1960f
Alan Gamboa <alangam97@gmail.com>
2021-01-06 16:50:11 -0600

    ya puede subir imagenes

--- app/src/main/java/com/example/nutritionapp/PostActivity.java
+++ app/src/main/java/com/example/nutritionapp/PostActivity.java
@@ -1,23 +1,41 @@
 package com.example.nutritionapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
 
     private ImageButton mselectImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+
+    private StorageReference mStorage;
+    Uri mImageURI = null;
+    private ProgressDialog mProgress;
+   
+
+
 
     private static final int GALLERY_REQUEST = 1;
     @Override
@@ -27,7 +45,14 @@ public class PostActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+
         mselectImage = (ImageButton) findViewById(R.id.imageButton);
+
+        mPostTitle = (EditText) findViewById(R.id.titleEditText);
+        mPostDesc = (EditText) findViewById(R.id.desciption_editText);
+
+        mProgress = new ProgressDialog(this);
 
         mselectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,10 +68,29 @@ public class PostActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startPosting();
             }
         });
+    }
+
+    private void startPosting() {
+        mProgress.setMessage("Espera...");
+        mProgress.show();
+        String title_val = mPostTitle.getText().toString().trim();
+        String desc_val = mPostDesc.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageURI !=null){
+
+            StorageReference filepath = mStorage.child("Post_Image").child(mImageURI.getLastPathSegment());
+            filepath.putFile(mImageURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                    mProgress.dismiss();
+                }
+            });
+
+        }
     }
 
     @Override
@@ -54,8 +98,8 @@ public class PostActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
-            Uri imageUri = data.getData();
-            mselectImage.setImageURI(imageUri);
+            mImageURI = data.getData();
+            mselectImage.setImageURI(mImageURI);
 
         }
     }
