7ffe8b95fefbc5006bfc85dea72843bc25a1e27b
Alan Gamboa <alangam97@gmail.com>
2021-01-06 13:44:52 -0600

    ya puede seleccionar imagen

--- app/src/main/java/com/example/nutritionapp/PostActivity.java
+++ app/src/main/java/com/example/nutritionapp/PostActivity.java
@@ -1,23 +1,43 @@
 package com.example.nutritionapp;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
 
+    private ImageButton mselectImage;
+
+    private static final int GALLERY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mselectImage = (ImageButton) findViewById(R.id.imageButton);
+
+        mselectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleyIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleyIntent.setType("image/*");
+                startActivityForResult(galleyIntent, GALLERY_REQUEST);
+            }
+        });
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +47,16 @@ public class PostActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            mselectImage.setImageURI(imageUri);
+
+        }
     }
 }
