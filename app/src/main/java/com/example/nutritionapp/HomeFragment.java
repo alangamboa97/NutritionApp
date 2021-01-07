e14a7d811e117a34de93c28d7f29330b39695024
fernando-oligon <fernando.oliveros.glez@gmail.com>
2021-01-07 14:42:11 -0600

    Subir PDF a Database y Storage

--- app/src/main/java/com/example/nutritionapp/HomeFragment.java
+++ app/src/main/java/com/example/nutritionapp/HomeFragment.java
@@ -1,12 +1,32 @@
 package com.example.nutritionapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +75,84 @@ public class HomeFragment extends Fragment {
         }
     }
 
+
+    EditText editPDFName;
+    Button  btn_upload;
+
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        editPDFName = view.findViewById(R.id.textPDFName);
+        btn_upload = view.findViewById(R.id.btn_upload_pdf);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("upload_pdf");
+
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editPDFName.getText().toString().isEmpty()){
+                    editPDFName.setError("Escriba el titulo del pdf");
+                    editPDFName.setFocusable(true);
+                }
+                else{
+                    selectPDFFile();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void selectPDFFile() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"SELECCIONA ARCHIVO PDF"),12);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 12 && resultCode == RESULT_OK
+                && data !=null && data.getData()!=null){
+            uploadPDFFILE(data.getData());
+        }
+    }
+
+    private void uploadPDFFILE(Uri data) {
+
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Subiendo...");
+        progressDialog.show();
+        
+        StorageReference reference= storageReference.child("upload_pdf/"+System.currentTimeMillis()+".pdf");
+        reference.putFile(data)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        Task<Uri> uri = taskSnapshot.getStorage().getDownloadUrl();
+                        while(!uri.isComplete());
+                        Uri url = uri.getResult();
+
+                        uploadPDF uploadPDF = new uploadPDF(editPDFName.getText().toString(),url.toString());
+                        databaseReference.child(databaseReference.push().getKey()).setValue(uploadPDF);
+                        Toast.makeText(getActivity(),"FILE UPLOADED", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress =(100.0*snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                            progressDialog.setMessage("UPLOADED: "+(int)progress+"%");
+                    }
+                });
     }
 }
